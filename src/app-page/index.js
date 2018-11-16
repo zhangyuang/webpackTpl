bf25ffa67aad675799c3794f57288aafb1715da1
yuang.zya <yuang.zya@alibaba-inc.com>
2018-11-16 15:10:08 +0800

    feat: 添加vue-router

--- src/app-page/index.js
+++ src/app-page/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
 
+Vue.use(VueRouter)
 /* eslint-disable no-new */
 new Vue({
+  el: '#app',
   render: h => h(App)
-}).$mount('#app')
+})
